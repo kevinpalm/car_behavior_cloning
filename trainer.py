0b2f1d5569331a8b7167251a8c7be5e2b82aeb41
Kevin Palm <kevinpalm24@gmail.com>
2017-01-15 09:19:55 -0800

    implimented an example generator for training

--- trainer.py
+++ trainer.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
-
+from scipy.misc import imread
 
 class ExampleGenerator():
+	
 	
 	def __init__(self):
 		
@@ -11,19 +12,59 @@ class ExampleGenerator():
 		header=None,
 		names=["Center Image", "Left Image", "Right Image",
 		"Steering Angle", "Throttle", "Break", "Speed"])
+		
+		# Shuffle indexes in prep for the first epoch
+		self.shuffs = np.array(self.df.index)
+		np.random.shuffle(self.shuffs)
+		
+		# Start a count for n epochs
+		self.ecount = 0
+		
 	
 	def verify_df(self):
 		
 		# Check to make sure something was loaded
 		print(self.df.head())
 		
+		
 	def epoch_reset(self):
 		
 		# Get a freshly shuffled index list
-		self.shuffled = np.array(self.df.index)
-		np.random.shuffle(self.shuffled)
-		print(self.shuffled)
+		self.shuffs = np.array(self.df.index)
+		np.random.shuffle(self.shuffs)
+		print(self.shuffs)
+		
+		# Add a count
+		self.ecount += 1
+		
+	
+	def read_image(self, fileloc):
+		
+		# Read in the image
+		image = imread(fileloc)
+		
+		return image
+		
+	def n_examples(self, n):
+		
+		# Make sure we have enough examples left in this epoch
+		if len(self.shuffs) < n:
+			self.epoch_reset()
+		
+		# Pull the slice list from the shuffled indexes
+		islice = self.shuffs[:n]
+		self.shuffs = self.shuffs[n:]
+		
+		# Format the outputs
+		angles = self.df.loc[islice, "Steering Angle"].values
+		locations = self.df.loc[islice, "Center Image"].tolist()
+		images = np.array([self.read_image(x) for x in locations])
+		
+		return images, angles
+		
 
 
 gen = ExampleGenerator()
-gen.epoch_reset()
+images, angles = gen.n_examples(2)
+print(images.shape)
+print(angles)
