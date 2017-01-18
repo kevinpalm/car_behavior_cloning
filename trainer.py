70b12310a398adf539840d40ecd0375c9e51802e
Kevin Palm <kevinpalm24@gmail.com>
2017-01-18 12:10:03 -0800

    basic linear regression training implimented

--- trainer.py
+++ trainer.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import numpy as np
-from scipy.misc import imread
+from scipy.misc import imread, imresize
+from keras.models import Sequential
+from keras.layers.core import Dense, Flatten
+from sklearn.model_selection import train_test_split
 
 class ExampleGenerator():
-	
 	
 	def __init__(self):
 		
@@ -42,8 +44,10 @@ class ExampleGenerator():
 		
 		# Read in the image
 		image = imread(fileloc)
+		image = imresize(image, 0.1)
 		
 		return image
+		
 		
 	def n_examples(self, n):
 		
@@ -61,10 +65,53 @@ class ExampleGenerator():
 		images = np.array([self.read_image(x) for x in locations])
 		
 		return images, angles
+	
+	def all_examples(self):
+		
+		# Store all the angles and read images
+		angles = self.df["Steering Angle"].values
+		images = np.array([self.read_image(x) for x in
+		self.df["Center Image"]])
+		
+		return images, angles
+		
 		
 
+class NNet():
+	
+	def model_def(self):
+		
+		# Init model
+		model = Sequential()
+		
+		# First layer
+		model.add(Flatten(input_shape=(16, 32, 3)))
+		model.add(Dense(922, init='normal', activation='relu'))
+		
+		# Output layer
+		model.add(Dense(1, init='normal'))
+		
+		# Compile model
+		model.compile(loss='mean_squared_error', optimizer='adam',
+		metrics=["mean_absolute_error"])
+		
+		return model
+		
+	def full_train(self, train_x, test_x, train_y, test_y):
+		self.model = self.model_def()
+		self.model.fit(train_x, train_y,
+		batch_size=128, nb_epoch=10,
+        verbose=1, validation_data=(test_x, test_y))
+		score = self.model.evaluate(test_x, test_y, verbose=0)
+		print('Test score:', score[0])
+		print('Test mean absolute error:', score[1])
 
 gen = ExampleGenerator()
-images, angles = gen.n_examples(2)
-print(images.shape)
-print(angles)
+images, angles = gen.all_examples()
+train_x, test_x, train_y, test_y = train_test_split(images, angles)
+
+print(train_x.shape, train_y.shape)
+print(test_x.shape, test_y.shape)
+
+model = NNet()
+model.full_train(train_x, test_x, train_y, test_y)
